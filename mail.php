e5600e18ed8c460dbf890bc1e92f8daf365d4c9a
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-06-05 22:22:00 +0530

    update index

--- mail.php
+++ mail.php
@@ -12,7 +12,7 @@ $recipient = "suryakantthakur825@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href="index.html">Return Home</a>";
 ?>
 
 
